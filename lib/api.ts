ebf9f65d1fc854efc62260848bc2525ecd93803f
Viktor Tar <tarviky@gmail.com>
2026-05-26 23:14:52 +0300

    Initial commit - NoteHub Next.js app

---
+++ lib/api.ts
@@ -0,0 +1,55 @@
+import axios from "axios";
+import type { Note, NoteTag } from "@/types/note";
+
+const token = process.env.NEXT_PUBLIC_NOTEHUB_TOKEN;
+
+axios.defaults.baseURL = "https://notehub-public.goit.study/api";
+axios.defaults.headers.common.Authorization = `Bearer ${token}`;
+
+export interface FetchNotesParams {
+  page: number;
+  search: string;
+  perPage: number;
+}
+
+export interface FetchNotesResponse {
+  notes: Note[];
+  totalPages: number;
+}
+
+export interface CreateNoteData {
+  title: string;
+  content: string;
+  tag: NoteTag;
+}
+
+// GET notes
+export const fetchNotes = async ({
+  page,
+  search,
+  perPage,
+}: FetchNotesParams): Promise<FetchNotesResponse> => {
+  const { data } = await axios.get("/notes", {
+    params: { page, search, perPage },
+  });
+
+  return data;
+};
+
+// GET single note (SSR requirement)
+export const fetchNoteById = async (id: string): Promise<Note> => {
+  const { data } = await axios.get(`/notes/${id}`);
+  return data;
+};
+
+// CREATE
+export const createNote = async (note: CreateNoteData): Promise<Note> => {
+  const { data } = await axios.post("/notes", note);
+  return data;
+};
+
+// DELETE
+export const deleteNote = async (id: string): Promise<Note> => {
+  const { data } = await axios.delete(`/notes/${id}`);
+  return data;
+};
